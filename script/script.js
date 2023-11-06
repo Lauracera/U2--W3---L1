3d17f7dd4e2d12d351f3af67a72fa631681accbd
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-06 15:31:23 +0100

    form html, inizio es 2

--- script/script.js
+++ script/script.js
@@ -41,4 +41,16 @@ class User {
 users();
 
 
-// es.2
+// Es.2
+
+class Pet {
+    constructor(petName, ownerName, species, breed) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.species = species;
+        this.breed = breed;
+    }
+
+
+  
+}
